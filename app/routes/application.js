a77f0a4608b61ec70ada9cb1538a799ddaa20748
David Audran <da@iotdk.dk>
2017-11-15 15:40:20 +0100

    Is fine w api

---
+++ app/routes/application.js
@@ -0,0 +1,8 @@
+// import Ember from 'Ember';
+// import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin'
+//
+// export default Ember.Route.extend(ApplicationRouteMixin);
+ import Route from '@ember/routing/route';
+
+export default Route.extend({
+});
